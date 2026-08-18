ce7764eb07fdc860ed8f20d3c21c4f6fb55fd238
Иван Чухманов <oollvanolloo@yandex.ru>
2026-08-18 11:55:15 +0300

    Удалена common-jwt-library

--- common-jwt-library/src/main/java/ru/bank/exception/TokenExpirationException.java
+++
@@ -1,7 +0,0 @@
-package ru.bank.exception;
-
-public class TokenExpirationException extends JwtValidationException{
-    public TokenExpirationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
--- common-jwt-library/src/main/java/ru/bank/exception/TokenNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.bank.exception;
-
-public class TokenNotFoundException extends JwtValidationException{
-    public TokenNotFoundException(String message) {
-        super(message);
-    }
-}
